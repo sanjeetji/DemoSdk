832b3279ff79ad79928abee96e510414acb00594
sanjeet_kumar <sanjeet.kumar@togopool.com>
2025-02-14 11:35:45 +0530

    remove extra code

--- demoSdk/src/main/java/com/sanjeet/demosdk/MySdkMainActivity.kt
+++ demoSdk/src/main/java/com/sanjeet/demosdk/MySdkMainActivity.kt
@@ -11,10 +11,5 @@ class MySdkMainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_my_sdk_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
     }
 }
