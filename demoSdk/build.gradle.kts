bd67df35dde4da7d76de151555b853f11893f571
sanjeet_kumar <sanjeet.kumar@togopool.com>
2025-02-14 12:06:09 +0530

    add version 1.0.5

--- demoSdk/build.gradle.kts
+++ demoSdk/build.gradle.kts
@@ -53,7 +53,7 @@ afterEvaluate {
                 from(components["release"])
                 groupId = "com.github.sanjeetji"
                 artifactId = "demosdk"
-                version = "1.0.0"
+                version = "1.0.5"
             }
         }
     }
